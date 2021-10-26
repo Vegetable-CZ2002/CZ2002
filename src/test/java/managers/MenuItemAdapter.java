54679c935e3b29538af2a28559539ec710e4e4a5
Rocket-Z <2395531943@qq.com>
2021-10-26 15:35:05 +0800

    adapters

---
+++ src/test/java/managers/MenuItemAdapter.java
@@ -0,0 +1,49 @@
+package managers;
+
+import java.lang.reflect.Type;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+
+import beans.MenuItem;
+
+public class MenuItemAdapter implements JsonSerializer<MenuItem>, JsonDeserializer<MenuItem>{
+
+    private static final String CLASSNAME = "MenuItem";
+    private static final String INSTANCE  = "INSTANCE";
+
+    @Override
+    public JsonElement serialize(MenuItem src, Type typeOfSrc,
+            JsonSerializationContext context) {
+
+        JsonObject retValue = new JsonObject();
+        String className = src.getClass().getName();
+        retValue.addProperty(CLASSNAME, className);
+        JsonElement elem = context.serialize(src); 
+        retValue.add(INSTANCE, elem);
+        return retValue;
+    }
+
+    @Override
+    public MenuItem deserialize(JsonElement json, Type typeOfT,
+    JsonDeserializationContext context) throws JsonParseException  {
+        JsonObject jsonObject = json.getAsJsonObject();
+        JsonPrimitive prim = (JsonPrimitive) jsonObject.get(CLASSNAME);
+        String className = prim.getAsString();
+
+        Class<?> klass = null;
+        try {
+            klass = Class.forName(className);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            throw new JsonParseException(e.getMessage());
+        }
+        return context.deserialize(jsonObject.get(INSTANCE), klass);
+    }
+}
