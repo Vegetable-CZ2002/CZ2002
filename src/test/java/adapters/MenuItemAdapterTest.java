d42d8ed41d9f0d36ec71d2f404e50fa505958662
Ruan Donglin <RUAN0018@e.ntu.edu.sg>
2021-10-26 15:54:54 +0800

    add adapter test, wait for zyx

---
+++ src/test/java/adapters/MenuItemAdapterTest.java
@@ -0,0 +1,14 @@
+package adapters;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MenuItemAdapterTest {
+
+    @org.junit.jupiter.api.Test
+    void serialize() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deserialize() {
+    }
+}
